670e97caf86ce11a3c6fad301f2f73e0c089e3f9
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-05-06 02:01:21 +0900

    Ver1

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = 'NDQyMjUxMzI5OTg1ODM5MTA0.Dc8RJw.a32WaDN4-wV3SXootMlk71XZtv8';
+const prefix = '!';
+
+client.on('ready', () => {
+    console.log('I am ready!');
+  });
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find('name', '⚽главная');
+    if (!channel) return;
+    channel.send(`Приветствуем в ги НАВЬ, ${member}`);
+  });
+
+client.on('message', message => {
+    console.log(message.content);
+    switch(message.content){
+        case prefix + 'h': message.reply("Список команд: \n!h - помощь\n"); break;
+        case prefix + 'rofl': 
+        /*var count = Math.floor(Math.random() * (client.users.array().length - 0)) + 0;
+        var stat;
+        if (Math.floor((Math.random() * (2 - 0)) + 0) == true){
+            stat = 'бомж!';
+        }
+        else stat = 'не бомж!';client.users.array()[count] + stat*/
+        message.reply(':P'); break;
+    }
+});
+
+
+
+client.login(token);
