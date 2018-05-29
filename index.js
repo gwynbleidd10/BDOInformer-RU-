37fee1755e6514e21c098569521c880aa8670a31
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-05-30 03:04:13 +0900

    status

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function dayNightTime(){
         else{
             endOfNight = 20 - time[2];
         }
-        client.user.setStatus(`Ночь. ${endOfDay[1]} ч. ${endOfDay[0]} мин.`);
+        client.user.setGame(`Ночь. ${endOfDay[1]} ч. ${endOfDay[0]} мин.`);
     }    
     else{
         if ((40 - time[2]) < 0){
@@ -80,7 +80,7 @@ function dayNightTime(){
         else{
             endOfDay[0] = 40 - time[2];
         }
-        client.user.setStatus(`День. ${endOfNight} мин.`);
+        client.user.setGame(`День. ${endOfNight} мин.`);
     }
 }
 
