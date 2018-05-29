36d2dae72f51ced4feecd25f3724a3224aadce60
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-05-29 18:31:23 +0900

    time

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('message', message => {
     console.log(message.content);
     switch(message.content){
         case prefix + 'h': message.reply("Список команд: \n!h - помощь\n"); break;
-        case prefix + 'resp': message.reply(d); break;
+        case prefix + 'resp': message.reply(boss); break;
         /*var count = Math.floor(Math.random() * (client.users.array().length - 0)) + 0;
         var stat;
         if (Math.floor((Math.random() * (2 - 0)) + 0) == true){
