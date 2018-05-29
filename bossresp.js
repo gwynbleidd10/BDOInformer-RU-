45ca7602a07768ca1b1b3785f5ae371319215c6f
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-05-29 19:13:09 +0900

    1

--- bossresp.js
+++ bossresp.js
@@ -1,6 +1,8 @@
 module.exports.boss = ['Каранда','Древень','Кзарка','Нубэр','Кутум','Трусливый бхег','Грязь','Красный нос'];
 
 module.exports.getTime = function(){
-    //day = new Date().getDay();
-    return `${new Date().getHours() + 3} : ${new Date().getMinutes()}`;
+    //var day = new Date().getDay();
+    var hour = new Date().getHours() + 3;
+    var minute = new Date().getMinutes(); 
+    return `${hour} : ${minute}`;
 };
