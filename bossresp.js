274a463829624ea116d6a1c622a4d52c7a784f20
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-05-29 18:47:30 +0900

    time

--- bossresp.js
+++ bossresp.js
@@ -5,9 +5,9 @@ getTime();
 var bosses = ['Каранда','Древень','Кзарка','Нубэр','Кутум','Трусливый бхег','Грязь','Красный нос'];
 
 function getTime(){
-    day = new Date().getDay();
-    time = new Date().getHours() + 3 + " : " + new Date().getMinutes();
+    //day = new Date().getDay();
+    return time = new Date().getHours() + 3 + " : " + new Date().getMinutes();
 };
 
-//console.log(time);
-module.exports = time;
+//console.log(getTime());
+module.exports = getTime();
