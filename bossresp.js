b0592026a1e1b4a85ac1fb7599ad9bfdb398e07e
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-05-29 19:20:50 +0900

    1

--- bossresp.js
+++ bossresp.js
@@ -1,6 +1,6 @@
 module.exports.boss = ['Каранда','Древень','Кзарка','Нубэр','Кутум','Трусливый бхег','Грязь','Красный нос'];
 
-module.exports.gt = function(){
+module.exports.getTime = function(x){
     //var day = new Date().getDay();
     var hour = new Date().getHours() + 3;
     var minute = new Date().getMinutes(); 
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('message', message => {
     console.log(message.content);
     switch(message.content){
         case prefix + 'h': message.reply("Список команд: \n!h - помощь\n"); break;
-        case prefix + 'resp': message.reply(boss.boss); break;
+        case prefix + 'resp': message.reply(boss.getTime(1)); break;
         /*var count = Math.floor(Math.random() * (client.users.array().length - 0)) + 0;
         var stat;
         if (Math.floor((Math.random() * (2 - 0)) + 0) == true){
