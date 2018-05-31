a1670047ceb4ab862e20efd470b902c3a5fa3bc1
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-06-01 04:36:40 +0900

    1

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ var boss = [
 function checkBoss(){    
     console.log("Проверка боссов выполнена");
     for(var i = 0; i < 5; i++){
-        console.log(i);
+        console.log(`${i},${tmpSep[0]}:${tmpSep[1]},${time[1]}:${time[2]}`);
         var tmpSep = boss[0][i].split(':');
         if ((`${time[1] + 1}:${time[2] - 30}` == boss[0][i]) || (`${time[1]}:${time[2] + 30}` == boss[0][i])){
             if (boss[time[0]][i] != ''){
